8801b48226d428700dca0c288e07c0cac4e30a32
urveshh <urveshbhesdadiya@gmail.com>
2025-02-17 00:45:48 +0530

    first commit

---
+++ node_modules/@igniteui/angular-templates/igx-ts-legacy/projects/_base/files/src/app/app.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AppComponent ],
+      imports: [ RouterTestingModule ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/environment/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: false,
+    CLOUDINARY_URL:'https://api.cloudinary.com/v1_1/dc4aobtkq/image/upload',
+    UPLOAD_PRESET:'CareerConnect',
+    API_URL:'http://localhost:3000',
+
+    CLIENT_ID :'523576895612-9kc3h4oq6hedhq2088jroe29heeqqv6n.apps.googleusercontent.com', 
+    BACKEND_URL:'http://localhost:5000',
+
+    ACCOUNT_SID :'ACe0b5aab97f4d89207cc8d18a87b6bc65',
+    AUTH_TOKEN: '1ffa4f81870a92def149a5084f42e80c',
+
+    SITE_KEY: '6LftUs0qAAAAAKZc0x0-In62vtiqtQI6mPGxtOm6'
+  };
